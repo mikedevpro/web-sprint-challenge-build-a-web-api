19634ac17fda6796ccdf45b2412705b648c59096
Michael Nobles <Mnobles33@gmail.com>
2025-03-25 06:39:40 -0700

    Last commit

--- api/actions/actions-middleware.js
+++ api/actions/actions-middleware.js
@@ -1,14 +1,3 @@
 // add middlewares here related to actions
-const Action = require('./actions-model');
 
-const requireNotes = (req, res, next) => {
-    if (req.body.notes) {
-        next();
-    } else {
-        res.status(400).json({ message: 'notes not present' })
-    }
-}
 
-module.export = {
-    requireNotes
-}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,42 +1,43 @@
 // Write your "actions" router here!
 const express = require('express');
 const Action = require('./actions-model');
-const { requireNotes } = require('./actions-middleware');
 const router = express.Router();
 
-router.get('/api/actions', (req, res) => {
-  Action.get()
-  .then(actions => {
-    res.status(200).json(actions)
-  })
-  .catch(error => {
-    res.status(500).json({ message: 'Server error -- could not get actions' })
-  })
-})
+router.get('/', async (req, res) => {
+  try {
+    const actions = await Action.get();
+    res.json(actions);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get actions' })
+  }
+});
 
-router.get('/api/actions/:id', (req, res) => {
-    Action.get(req.params.id)
-    .then(resp => {
-        if(resp) {
-            res.status(200).json(resp)
-        } else {
-            res.status(404).json({ message: 'There was not an action with that id' })
-        }
-    })
-    .catch(error => {
-        res.status(500).json({ message: 'Server error -- could not get the actions' })
-    })
-})
+router.get('/:id', async (req, res) => {
+  try {
+    const action = await Action.get(req.params.id);
+    if (action) {
+        res.json(action);
+    } else {
+        res.status(404).json({ message: 'Action not found' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to get action' })
+  }
+});
 
-router.post('/', (req, res) => {
-    Action.insert(req.body)
-    .then(resp => {
-        res.status(200).json(resp)
-    })
-    .catch(error => {
-        res.status(500).json({ message: 'Server error -- server could not add the action' })
-    })
-})
+router.post('/',  async (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    return res.status(400).json({ message: 'Project_id, description, and notes are required' });
+  }
+
+  try {
+    const newAction = await Action.insert(req.body);
+    res.status(201).json(newAction);
+  } catch (err) {
+    res.status(500).json({ message: 'Failed to create new action' });
+  }
+});
 
 router.put('/:id', (req, res) => {
     if(!req.body.project_id || !req.body.description || !req.body.notes) {
