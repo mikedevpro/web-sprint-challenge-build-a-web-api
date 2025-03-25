f22a842f0deb93f28def36381eb62b26d858b6f8
Michael Nobles <Mnobles33@gmail.com>
2025-03-24 17:12:33 -0700

    next commit

--- api/actions/actions-middleware.js
+++ api/actions/actions-middleware.js
@@ -1,1 +1,14 @@
 // add middlewares here related to actions
+const Action = require('./actions-model');
+
+const requireNotes = (req, res, next) => {
+    if (req.body.notes) {
+        next();
+    } else {
+        res.status(400).json({ message: 'notes not present' })
+    }
+}
+
+module.export = {
+    requireNotes
+}
